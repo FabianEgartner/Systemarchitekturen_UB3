393929ef1cc40d7da61615ae01b28a3504c06070
schlaumeierisch <matthias.meier97@hotmail.com>
2022-05-20 19:17:37 +0200

    further work

--- src/main/java/at/fhv/sysarch/lab3/pipeline/api/Filter.java
+++ src/main/java/at/fhv/sysarch/lab3/pipeline/api/Filter.java
@@ -6,9 +6,14 @@ import at.fhv.sysarch.lab3.pipeline.obj.Pipe;
 // TODO: The current solution is JUST an illustration and not sufficient for the example. It only shows how generics may be used.
 // TODO: Can you use one interface for both implementations (push and pull)? Or do they require specific interfaces?
 public interface Filter<I, O> {
+
+    I read();
+
     void write(I data);
 
-    void setPipeSuccessor(Pipe<O> pipe);
+    void setPipePredecessor(Pipe<O> predecessor);
+
+    void setPipeSuccessor(Pipe<O> successor);
 
     O process(I input);
 }
--- src/main/java/at/fhv/sysarch/lab3/pipeline/obj/Pipe.java
+++ src/main/java/at/fhv/sysarch/lab3/pipeline/obj/Pipe.java
@@ -5,13 +5,22 @@ import at.fhv.sysarch.lab3.pipeline.api.Filter;
 // TODO: how can pipes be used for different data types?
 public class Pipe<I> {
 
+    private Filter<I, ?> predecessor;
     private Filter<I, ?> successor;
 
-    public void setSuccessor (Filter<I, ?> successor) {
-        this.successor = successor;
+    public I read() {
+        return this.predecessor.read();
     }
 
     public void write(I data) {
         this.successor.write(data);
+    }
+
+    public void setPredecessor(Filter<I, ?> predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    public void setSuccessor (Filter<I, ?> successor) {
+        this.successor = successor;
     }
 }
